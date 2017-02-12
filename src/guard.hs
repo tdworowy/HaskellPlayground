0234d9a1ce2b7e9f37a0a3db6c54074f1941b3ec
Tomasz <dworowytomasz@gmail.com>
2017-02-12 13:25:23 +0100

    update

---
+++ src/guard.hs
@@ -0,0 +1,12 @@
+main =do
+    let guardFun [] = "empty list"
+        guardFun (x:_)
+            | x == 0 = "Zero"
+            | x > 0 && even x = "even"
+            | x > 0 && odd x = "odd"
+            | otherwise = "negative"
+    print(guardFun [])
+    print(guardFun [1,2,3])
+    print(guardFun [2,4,2])
+    print(guardFun [0,100,2])
+    print(guardFun [-1,2,3])
