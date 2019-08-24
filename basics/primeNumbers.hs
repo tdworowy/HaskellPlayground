a25341f0b5ee2811de3cbd610d4709c31abb9ef7
Tomasz <dworowytomasz@gmail.com>
2019-08-24 14:07:55 +0200

    update

--- basics/primeNumbers.hs
+++
@@ -1,9 +0,0 @@
-import Data.Numbers.Primes
-main :: IO ()
-main = do
-    putStrLn "Enter Number: "
-    numberStr <-getLine
-    let n = read $ numberStr ::Int
-    let primeNumber = head . drop (n -1) $ primes
-putStrLn $ "Prime number for " ++ show n ++ "is" ++ show primeNumber
--- not work yet
